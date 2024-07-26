7e9bef3a127f734a25aafad2b8e12539a87bc45d
AbdullahOmran <abdullahomrana12312332110h@gmail.com>
2024-07-26 22:18:35 +0300

    updated

--- app/core/security.py
+++ app/core/security.py
@@ -19,7 +19,7 @@ def create_access_token(data: dict, expires_delta: Union[timedelta, None] = None
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=15)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
 
